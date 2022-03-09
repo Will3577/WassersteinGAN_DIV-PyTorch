a7514f691a5660f291e039c766ee66f215b7bc87
Hengrui  Wang <u6202156@anu.edu.au>
2022-03-09 11:16:49 +1100

    test new decoder

--- trainer.py
+++ trainer.py
@@ -50,20 +50,21 @@ class Trainer(object):
         if args.dataset in ["imagenet", "folder", "lfw"]:
             # folder dataset
            
-            dataset = SliceDataset(args.data)
             # dataset = SliceDataset(args.data)
-            # dataset = torchvision.datasets.ImageFolder(root=args.data,
-            #                                            transform=transforms.Compose([
-            #                                                 RandomResize(),
-            #                                         #     #    transforms.Resize((args.image_size, args.image_size)),
-            #                                         #     #    transforms.CenterCrop(args.image_size),
-            #                                                 transforms.RandomHorizontalFlip(p=0.5),
-            #                                                 transforms.RandomVerticalFlip(p=0.5),
+            # dataset = SliceDataset(args.data)
+            dataset = torchvision.datasets.ImageFolder(root=args.data,
+                                                       transform=transforms.Compose([
+                                                            # RandomResize(),
+                                                    #     #    transforms.Resize((args.image_size, args.image_size)),
+                                                    #     #    transforms.CenterCrop(args.image_size),
+                                                            transforms.RandomHorizontalFlip(p=0.5),
+                                                            transforms.RandomVerticalFlip(p=0.5),
+                                                            transforms.RandomResizedCrop(256),
 
-            #                                                 transforms.ToTensor(),
-            #                                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-            #                                            ])
-            #                                            )
+                                                            transforms.ToTensor(),
+                                                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                                       ])
+                                                       )
         elif args.dataset == "lsun":
             classes = [c + "_train" for c in args.classes.split(",")]
             dataset = torchvision.datasets.LSUN(root=args.data, classes=classes,
